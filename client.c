a6d891fe8a3a41b04cce4d2271de02677edfe8a9
0-Tenkai-0 <nico.collignon0@gmail.com>
2021-01-04 09:52:56 +0100

    version 1

---
+++ client.c
@@ -0,0 +1,39 @@
+// librairies client
+#ifdef __WIN32__
+#include <winsock2.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    //mise en place du seveur
+    int socket_serveur = socket (AF_INET,SOCK_STREAM,0);
+
+    // mise en place de l'adresse
+    struct sockaddr_in adr_serv;
+    adr_serv.sin_family = AF_INET;
+    adr_serv.sin_port = htons(2569);
+    adr_serv.sin_addr.s_addr = INADDR_ANY;
+
+    //initier connexion
+    int etat_connect = connect (socket_serveur, (struct sockaddr *) &adr_serv, sizeof(adr_serv));
+    //vérifier connexion
+    if (etat_connect == -1) {
+        printf("Erreur de connexion \n");
+    }
+    char mess_serveur [256];
+    char* stock_mess_cl;
+    printf("Vous êtes bien connecté");
+    scanf("%s ", stock_mess_cl);
+
+    write(socket_serveur, stock_mess_cl, sizeof(stock_mess_cl));
+
+    return 0;
+}
+
---
+++ serveur.c
@@ -0,0 +1,53 @@
+// librairies serveurs
+#ifdef __WIN32__
+#include <winsock2.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+
+void* gestion_connect(void* psocket_client);
+
+int main() {
+    //mise en place du seveur
+    int socket_serveur = socket (AF_INET,SOCK_STREAM,0);
+
+    // mise en place de l'adresse
+    struct sockaddr_in adr_serv;
+    adr_serv.sin_family = AF_INET;
+    adr_serv.sin_port = htons(2569);
+    adr_serv.sin_addr.s_addr = INADDR_ANY;
+
+    // connecter le socket a l'ip et le port
+    bind(socket_serveur, (struct sockaddr *) &adr_serv, sizeof(adr_serv));
+
+    listen(socket_serveur,100);
+
+    int socket_client;
+    //gérer l'arriver des clients
+    while((socket_client = accept(socket_serveur, NULL, NULL))){
+        pthread_t th_client;
+        pthread_create(&th_client, NULL, ??, &socket_client);
+    }
+    close (socket_serveur);
+
+    return 0;
+}
+
+void* gestion_connect(void* psocket_client){
+    //récupération socket client
+    int socket_client = *((int *)psocket_client);
+    char mess_client [256];
+    printf("Le client est bien connecté");
+
+    read(socket_client, mess_client, 256);
+    printf("%s ", mess_client);
+}
