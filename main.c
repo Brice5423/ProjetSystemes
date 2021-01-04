3b7347328ebd1b8e9bafa084d8b7e686036f0301
0-Tenkai-0 <nico.collignon0@gmail.com>
2021-01-04 09:08:14 +0100

    libraires

--- main.c
+++ main.c
@@ -2,3 +2,32 @@
 // Created by brice on 14/12/2020.
 //
 
+// librairies client
+#ifdef __WIN32__
+#include <winsock2.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+// librairies serveurs
+#ifdef __WIN32__
+#include <winsock2.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
